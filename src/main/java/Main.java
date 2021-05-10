53337f76935e83197c8d3fc8cf6713763d669406
Mateusz <74718131+MateuszBilik@users.noreply.github.com>
2021-05-10 15:20:18 +0200

    counts

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,12 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-       String s = new Sentence().getSentence();
-       List<String> s2 = new Sentence().removeSpecialChars(s);
+        Sentence sentence = new Sentence();
+        String s = sentence.getSentence();
+        List<String> s2 = sentence.removeSpecialChars(s);
+        int i = sentence.count(s2);
+        int j = sentence.countSpecial(s);
+        System.out.println(s + s2 + i + j);
+
     }
 }
--- src/main/java/Sentence.java
+++ src/main/java/Sentence.java
@@ -1,9 +1,16 @@
-import java.io.*;
+import org.apache.log4j.Logger;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
 
 public class Sentence {
+
+    Logger log = Logger.getLogger(getClass());
 
     public String getSentence() throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in)); //TODO more line
@@ -11,16 +18,46 @@ public class Sentence {
     }
 
     public List<String> removeSpecialChars(String text) throws IOException {
-        Properties prop = new Properties();
-        prop.load(new FileInputStream("src/main/resources/config.properties"));
+        Properties prop = getProp();
         String specialChars = prop.getProperty("special").replace(" ", "");
         List<String> specialsList = Arrays.asList(specialChars.split(""));
-        text.trim();
-        text.replaceAll("\\s+", " ");
+        text = text.trim();
+        text = text.replaceAll("\\s+", " ");
 
         for (String s : specialsList) {
             text = text.replace(s, "");
         }
         return Arrays.asList(text.split(" "));
+    }
+
+    private Properties getProp() throws IOException {
+        Properties prop = new Properties();
+        prop.load(new FileInputStream("src/main/resources/config.properties"));
+        return prop;
+    }
+
+    public int count(List<String> texts) {
+        int count = 0;
+        for (String s : texts) {
+            count += s.length();
+        }
+        return count;
+    }
+
+    public int countSpecial(String text) throws IOException {
+        Properties prop = getProp();
+        String[] searchChars = prop.getProperty("logic").toLowerCase().split("");
+        text = text.toLowerCase();
+        String withoutSearchChars = text;
+        for (String s : searchChars) {
+            withoutSearchChars = withoutSearchChars.replaceAll(s, "");
+        }
+        return text.length() - withoutSearchChars.length();
+    }
+
+
+
+    public void printRapport () {
+
     }
 }
