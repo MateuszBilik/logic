3b38a4a7c708d982eb44f8ba44d9b6da006c03ff
Mateusz <74718131+MateuszBilik@users.noreply.github.com>
2021-05-10 17:51:47 +0200

    is ok, i need sort

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,11 +5,12 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         Sentence sentence = new Sentence();
-        String s = sentence.getSentence();
-        List<String> s2 = sentence.removeSpecialChars(s);
-        int i = sentence.count(s2);
-        int j = sentence.countSpecial(s);
-        System.out.println(s + s2 + i + j);
+//        String s = sentence.getSentence();
+//        List<String> s2 = sentence.removeSpecialChars(s);
+//        int i = sentence.countChars(s2);
+//        int j = sentence.countAllSpecials(s);
+//        System.out.println(s + s2 + i + j);
 
+        sentence.printRapport();
     }
 }
--- src/main/java/Sentence.java
+++ src/main/java/Sentence.java
@@ -4,39 +4,47 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Properties;
+import java.util.*;
 
 public class Sentence {
 
     Logger log = Logger.getLogger(getClass());
+    Properties prop = getProp();
 
-    public String getSentence() throws IOException {
+    public String getSentence() {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in)); //TODO more line
-        return reader.readLine();
+        try {
+            return reader.readLine();
+        } catch (IOException e) {
+            log.error("Something wrong with command: " + e.getMessage());
+            return "";
+        }
     }
 
     public List<String> removeSpecialChars(String text) throws IOException {
-        Properties prop = getProp();
         String specialChars = prop.getProperty("special").replace(" ", "");
         List<String> specialsList = Arrays.asList(specialChars.split(""));
-        text = text.trim();
-        text = text.replaceAll("\\s+", " ");
 
         for (String s : specialsList) {
             text = text.replace(s, "");
         }
+        text = text.trim();
+        text = text.replaceAll("\\s+", " ");
+
         return Arrays.asList(text.split(" "));
     }
 
-    private Properties getProp() throws IOException {
+    private Properties getProp() {
         Properties prop = new Properties();
-        prop.load(new FileInputStream("src/main/resources/config.properties"));
+        try {
+            prop.load(new FileInputStream("src/main/resources/config.properties"));
+        } catch (IOException e) {
+            log.error("Can't find properties file: " + e.getMessage());
+        }
         return prop;
     }
 
-    public int count(List<String> texts) {
+    public int countChars(List<String> texts) {
         int count = 0;
         for (String s : texts) {
             count += s.length();
@@ -44,9 +52,8 @@ public class Sentence {
         return count;
     }
 
-    public int countSpecial(String text) throws IOException {
-        Properties prop = getProp();
-        String[] searchChars = prop.getProperty("logic").toLowerCase().split("");
+    public int countAllSpecials(String text) throws IOException {
+        String[] searchChars = arraySpecial();
         text = text.toLowerCase();
         String withoutSearchChars = text;
         for (String s : searchChars) {
@@ -55,9 +62,47 @@ public class Sentence {
         return text.length() - withoutSearchChars.length();
     }
 
+    private List<String> countSpecialsInWord(String text) throws IOException {
+        String[] searchChars = arraySpecial();
+        text = text.toLowerCase();
+        String withoutSpecialChars = text;
+        List<String> response = new ArrayList<>();
+        response.add(0, "");
+        for (String s : searchChars) {
+            String withoutS = text;
+            withoutS = withoutS.replaceAll(s, "");
+            withoutSpecialChars = withoutSpecialChars.replaceAll(s, "");
 
+            int i = 0;
+            if ((i = text.length() - withoutS.length()) != 0) {
+                response.set(0, response.get(0) + s);
+            }
+        }
+        response.add(1, String.valueOf(text.length() - withoutSpecialChars.length()));
+//        System.out.println(response);
+        return response;
+    }
 
-    public void printRapport () {
+    private String[] arraySpecial() {
+        return prop.getProperty("logic").toLowerCase().split("");
+    }
+
+    public void printRapport() throws IOException {
+        String sentence = getSentence();
+        int specialCharsInSentence = countAllSpecials(sentence);
+        List<String> words = removeSpecialChars(sentence);
+
+        for (String s : words){
+            List<String> wordList = countSpecialsInWord(s);
+            double part = Integer.parseInt(wordList.get(1)) * 100d / specialCharsInSentence;
+            part = Math.round(part);
+            System.out.println(
+                    "("+ Arrays.asList(wordList.get(0).split("")).toString() +
+                    ", " + s.length() +
+                    ") = " + part/100 +
+                    " (" +  wordList.get(1) + "/" + specialCharsInSentence + ")" + "word: "+ s);
+        }
+
 
     }
 }
